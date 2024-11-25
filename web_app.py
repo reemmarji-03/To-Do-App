2121305f52532036271d3b31f2346d45a68769a2
AzureAD\ReemMarji <reem.marji@gotocme.com>
2024-11-25 11:32:41 +0200

    finalizing web app

--- web_app.py
+++ web_app.py
@@ -1,16 +1,28 @@
 import streamlit as st 
 import functions
 
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"]
+    todos.append(todo+"\n")
+    functions.write_todos(todos)
 
 st.title("My Todo App")
 st.subheader("Welcome to your to-do list")
 
 
-todos = functions.get_todos()
 for todo in todos:
-    st.checkbox(todo)
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.remove(todo)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun
+    
 
-st.text_input(label= "Enter a todo:", placeholder="Add a todo")
+st.text_input(label= "Enter a todo:", placeholder="Add a todo...",
+              on_change=add_todo, key="new_todo")
 
 
 
